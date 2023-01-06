a13fbf0bf8345ab975b08174eee3673ca2905838
Abdul khan <abdulmatania@gmail.com>
2023-01-06 22:47:06 +0530

    Added Dark mode and minor changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,11 @@ import PropTypes from 'prop-types';
 
 let Navbar = (props) => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
             <a className="navbar-brand" href="/">{props.title}</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
@@ -18,9 +17,9 @@ let Navbar = (props) => {
                     </li>
                 </ul>
             </div>
-            <div className="custom-control custom-switch">
-                <input type="checkbox" className="custom-control-input" id="customSwitch1" />
-                <label className="custom-control-label" htmlFor="customSwitch1">Toggle this switch element</label>
+            <div className={`custom-control custom-switch text-${props.mode==='light'?'dark':'light'}`}>
+                <input type="checkbox" onClick={props.toggleMode} className="custom-control-input" id="customSwitch1" />
+                <label className="custom-control-label" htmlFor="customSwitch1">Enable/Disable Dark Mode</label>
             </div>
         </nav>
     )
@@ -33,6 +32,6 @@ Navbar.propTypes = {
 
 Navbar.defaultProps = {
     title: 'set title here'
-}
+};
 
 export default Navbar;    
